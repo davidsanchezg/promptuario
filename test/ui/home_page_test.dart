7a9b28863a190a2d59cb921bd5de8d9bcabbfda2
David S <ing.davi@gmail.com>
2025-06-12 23:39:24 -0400

    fix: update test imports and CI workflow to fix test failures

--- test/ui/home_page_test.dart
+++ test/ui/home_page_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail_image_network/mocktail_image_network.dart';
+import 'package:promptuario/ui/home_page.dart';
 import 'package:promptuario/ui/widgets/product_card.dart';
 
 void main() {
